f732664f916937ae49f76453f31ae07146de3140
pedroblanch <pedroblanch@iesjulioverne.es>
2022-01-17 11:14:45 +0100

    Lunes 17 de enero - 2

--- sakila/verPagos.php
+++ sakila/verPagos.php
@@ -4,22 +4,28 @@ if ($conexion->connect_errno) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "select title, release_year from film";
+$sql = "select first_name, last_name, payment_date, amount from
+	customer c, payment p
+		where p.customer_id=c.customer_id
+			order by payment_date;";
 $result = $conexion->query($sql);
+$total=0;
 
-if ($result->num_rows > 0) {
-    echo "<table border=1>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $title=$row['title'];
-        $release_year=$row['release_year'];
-        echo "<tr>";
-        echo "<td>$title</td><td>$release_year</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
+echo "<table border=1>";
+// output data of each row
+while ($row = $result->fetch_assoc()) {
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    $payment_date = $row['payment_date'];
+    $amount = $row['amount'];
+    $total=$total+$amount;
+    echo "<tr>";
+    echo "<td>$last_name</td><td>$first_name</td>";
+    echo "<td>$payment_date</td><td align=right>$amount</td>";
+    echo "</tr>";
+}//end_while
+echo "<tr><td colspan=3 align=right>Total:</td><td align=right>$total</td></tr>";
+echo "</table>";
+
 $conexion->close();
 ?>
