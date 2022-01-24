6e0726c0e43f4adaeb4a739a04a32f7569a9b192
pedroblanch <pedroblanch@iesjulioverne.es>
2022-01-24 11:12:33 +0100

    Lunes 24 de enero

---
+++ sakila/actoresPelicula2.php
@@ -0,0 +1,46 @@
+<?php
+$conexion = new mysqli("10.10.0.3", "sakila", "sakila", "sakila", 3306);
+if ($conexion->connect_errno) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if (! isset($_GET['film_id'])) {
+    die('falta el parámetro film_id');
+}
+if (empty($_GET['film_id'])) {
+    die('falta el parámetro film_id');
+}
+$film_id = $_GET['film_id'];
+
+$sql = "select title from film where film_id=$film_id";
+$result = $conexion->query($sql);
+if ($row = $result->fetch_assoc()) {
+    $title = $row['title'];
+} else {
+    die("No se encuentra la película");
+}
+echo "<h1>$title</h1>";
+?>
+<label for='actor_id'>Actor:</label>
+<form action='actoresPelicula3.php' method='get'>
+	<select name='actor_id'>
+		<option value=''></option>
+    	<?php        
+        $sql2 = "select a.actor_id,last_name, first_name from actor a, film_actor fa
+        	where fa.actor_id=a.actor_id 
+        		and fa.film_id=$film_id
+                    order by last_name, first_name";
+        $result2 = $conexion->query($sql2);
+        // output data of each row
+        while ($row2 = $result2->fetch_assoc()) {
+            $actor_id = $row2['actor_id'];
+            $last_name = $row2['last_name'];
+            $first_name = $row2['first_name'];
+            echo "<option value='$actor_id'>$last_name, $first_name</option>";
+        }
+        $conexion->close();
+        ?>	
+	</select>
+	<br>
+	<input type='submit' value='Aceptar'></input>
+</form>
+
