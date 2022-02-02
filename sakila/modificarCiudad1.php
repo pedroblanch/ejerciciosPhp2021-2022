16ec393de16ad7b523d72a3ac4ee5f5f04d473b2
ppblanchleiva <ppblanchleiva@gmail.com>
2022-02-02 12:42:26 +0100

    Miércoles 2 de febrero

---
+++ sakila/modificarCiudad1.php
@@ -0,0 +1,26 @@
+<?php
+$conexion = new mysqli("localhost", "sakila", "sakila", "sakila", 3306);
+if ($conexion->connect_errno) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select city_id, city from city order by city;";
+$result = $conexion->query($sql);
+?>
+<form method="get" action="modificarCiudad2.php">
+	<label for="city_id">Ciudad:</label> 
+	<select name="city_id">
+		<option value=''></option>
+        <?php 
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            $city_id = $row['city_id'];
+            $city = $row['city'];
+            echo "<option value='$city_id'>$city</option>";
+        }
+        $conexion->close();
+        ?>
+	</select> 
+	<br>
+	<input type="submit" value="aceptar">
+</form>
---
+++ sakila/modificarCiudad2.php
@@ -0,0 +1,48 @@
+<?php
+$conexion = new mysqli("localhost", "sakila", "sakila", "sakila", 3306);
+if ($conexion->connect_errno) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if (! isset($_GET['city_id'])) {
+    die('falta el parámetro city_id');
+}
+if (empty($_GET['city_id'])) {
+    die('falta el parámetro city_id');
+}
+$city_id = $_GET['city_id'];
+
+$sql = "select city, country_id  from city where city_id=$city_id;";
+$result = $conexion->query($sql);
+// output data of each row
+$row = $result->fetch_assoc();
+if (! $row) {
+    die("No existe ciudad con ese código");
+}
+$city = $row['city'];
+$country_id = $row['country_id'];
+echo("country_id:  $country_id<br>");
+$sql2 = "select country_id as country_id_select, country from country order by country;";
+$result2 = $conexion->query($sql2);
+?>
+<form action="modificarCiudad3.php" method="get">
+	<label for="city">Ciudad:</label> <input type="text" name='city'
+		value="<?=$city?>"> <br> <label for="country_id">Pais:</label> <select
+		name="country_id">
+		<option value=''></option>
+        <?php
+        // output data of each row
+        while ($row2 = $result2->fetch_assoc()) {
+            $country_id_select = $row2['country_id_select'];
+            $country = $row2['country'];
+            if ($country_id == $country_id_select) {
+                echo "<option value='$country_id_select' selected>$country</option>";
+            }
+            else{
+                echo "<option value='$country_id_select' >$country</option>";
+            }
+        }
+        $conexion->close();
+        ?>
+	</select> <br> <input type="hidden" name="city_id"
+		value='<?=$city_id?>'> <br> <input type='submit' value='Aceptar'>
+</form>
